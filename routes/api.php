82bfa1af21237f57cf25ee2c03ebecc6b50dfe51
salmanlfarisi <alfarissalman2419@gmail.com>
2025-06-04 06:51:11 +0700

    Tambah middleware admin untuk route genres dan authors

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,19 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\AuthorController;
 
+// Route buku tetap seperti sebelumnya
 Route::get('/books', [BookController::class, 'showBooks']);
 Route::get('/books/{id}', [BookController::class, 'showBook']);
 Route::post('/books', [BookController::class, 'storeBook']);
 Route::put('/books/{id}', [BookController::class, 'updateBook']);
 Route::delete('/books/{id}', [BookController::class, 'deleteBook']);
 
-Route::apiResource('genres', GenreController::class);
-Route::apiResource('authors', AuthorController::class);
+// Routes publik untuk genres dan authors (bisa index & show)
+Route::apiResource('genres', GenreController::class)->only(['index', 'show']);
+Route::apiResource('authors', AuthorController::class)->only(['index', 'show']);
+
+// Routes yang butuh autentikasi & admin untuk create, update, delete
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::apiResource('genres', GenreController::class)->only(['store', 'update', 'destroy']);
+    Route::apiResource('authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+});
